4d6c54590158314fbbdc980c127b8a78a4e14490
Connor Burns <ctrekker4@gmail.com>
2021-02-11 16:46:49 -0700

    Config custom fetch method

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,7 +1,21 @@
 const Config = {
     backendUrl: 'http://localhost:8000',
 };
-Config.endpoint = (suffix) => Config.backendUrl + '/api' + suffix
-
+Config.endpoint = (suffix) => Config.backendUrl + '/api' + suffix;
+Config.fetch = (endpoint, opts={
+    method: 'GET'
+}) => {
+    return new Promise((resolve, reject) => {
+        fetch(Config.endpoint(endpoint), {
+            credentials: 'include',
+            headers: {
+                'content-type': 'application/json'
+            },
+            ...opts
+        }).then(res => res.json())
+        .then(resolve)
+        .catch(reject);
+    });
+};
 
 export default Config;
