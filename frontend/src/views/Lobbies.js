4905f0996367fcabd03bbea8cc3f91505cd53921
Ianomatic1 <ianomatic1@gmail.com>
2021-02-20 22:00:00 -0700

    Hotfix to make home and lobbies page actually work

--- frontend/src/views/Lobbies.js
+++ frontend/src/views/Lobbies.js
@@ -48,7 +48,7 @@ export default function Lobbies() {
             credentials: 'include'
         }).then(res => res.json())
         .then(res => {
-            setLobbies(res);
+            setLobbies(Object.values(res));
         }).catch(err => {
             if(err) console.log(err);
         });
