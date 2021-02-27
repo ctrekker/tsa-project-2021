143cf741ea2f32005fe22d3cb3ee641a12734c6d
Ianomatic1 <ianomatic1@gmail.com>
2021-02-27 13:23:10 -0700

    temp commit to pull

--- frontend/src/components/ClassPreview.js
+++ frontend/src/components/ClassPreview.js
@@ -2,35 +2,41 @@ import { Card, CardActionArea, Typography } from '@material-ui/core';
 import React from 'react'
 import './ClassPreview.css';
 import moment from 'moment';
+import {Link as RouterLink} from 'react-router-dom';
+import './LobbyPost.css';
+
 
 export default function ClassPreview(props) {
     const {
-        CLASS_NAME,
-        INSTRUCTOR_NAME,
-        CLASS_RATING,
-        SCHEDULED_FOR,
-        onClick
+        class_name,
+        instructor_name,
+        class_rating,
+        scheduled_for,
+        class_id,
+        lobby_id
     } = props;
 
     return (
         <Card>
-            <CardActionArea onClick={onClick}>
-                <div className="container">
-                    <div className="left">
-                        <Typography variant="subtitle1" className="ttext" style={{marginTop: 0}}>{CLASS_NAME}</Typography>
-                        <Typography variant="p" id="author">by {INSTRUCTOR_NAME} ({CLASS_RATING})</Typography>
+            <RouterLink className="userLink" to={`lobby/${lobby_id}/class/${class_id}`}>
+                <CardActionArea>
+                    <div className="container">
+                        <div className="left">
+                            <Typography variant="subtitle1" className="ttext" style={{marginTop: 0}}>{class_name}</Typography>
+                            <Typography variant="p" id="author">by {instructor_name} ({class_rating})</Typography>
+                        </div>
+
+                        <div style={{flexGrow: 1}}/>
+
+                        <div className="right">
+                            <Typography variant="p" id="text" color="textSecondary">Scheduled For:</Typography>
+                            <Typography variant="subtitle2" className="date">{moment(scheduled_for).format('MMM Do')}</Typography>
+                            <Typography variant="subtitle2" className="date">{moment(scheduled_for).format('h:mm A')}</Typography>
+                        </div>
                     </div>
 
-                    <div style={{flexGrow: 1}}/>
-
-                    <div className="right">
-                        <Typography variant="p" id="text" color="textSecondary">Scheduled For:</Typography>
-                        <Typography variant="subtitle2" className="date">{moment(SCHEDULED_FOR).format('MMM Do')}</Typography>
-                        <Typography variant="subtitle2" className="date">{moment(SCHEDULED_FOR).format('h:mm A')}</Typography>
-                    </div>
-                </div>
-
-            </CardActionArea>
+                </CardActionArea>
+            </RouterLink>
         </Card>
     );
 }
