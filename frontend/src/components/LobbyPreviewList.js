0281a54ad45b5ad885d88367ef71e533ecc957ea
Connor Burns <ctrekker4@gmail.com>
2021-03-01 19:00:36 -0700

    Fix duplicate categories and add lobby images to home

--- frontend/src/components/LobbyPreviewList.js
+++ frontend/src/components/LobbyPreviewList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {Grid} from "@material-ui/core";
 import LobbyPreview from "./LobbyPreview";
+import Config from '../Config';
 
 export default function LobbyPreviewList({lobbies, scaler = 1}){
     return(
         <Grid container spacing={Math.round(4 * scaler)} style={{padding: 50 * scaler}}>
             {lobbies.map((lobby, i) => ((
                 <Grid item sm={12} md={Math.round(6 * scaler)} lg={Math.round(4 * scaler)}>
-                    <LobbyPreview {...lobby}/>
+                    <LobbyPreview {...lobby} picture={`${Config.endpoint(`/lobbies/${lobby.id}/image`)}`}/>
                 </Grid>
             )))}
         </Grid>
